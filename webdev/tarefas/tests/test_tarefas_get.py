352f12d594d5235bd29e0119c2f4a63be8fed1d3
Victor Augusto <victor.augusto60@gmail.com>
2021-03-24 10:21:24 -0300

    Implementado Listar tarefas pendentes

--- webdev/tarefas/tests/test_tarefas_get.py
+++ webdev/tarefas/tests/test_tarefas_get.py
@@ -2,9 +2,11 @@ import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
+from webdev.tarefas.models import Tarefa
+
 
 @pytest.fixture
-def resposta(client):
+def resposta(client, db):
     resp = client.get(reverse('tarefas:home'))
     return resp
 
@@ -19,3 +21,24 @@ def test_formulario_presente(resposta):
 
 def test_botao_salvar_presente(resposta):
     assertContains(resposta, 'button type="submit"')
+
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Tarefa 1', feita=False),
+        Tarefa(nome='Tarefa 2', feita=False),
+    ]
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+
+@pytest.fixture
+def resposta_com_lista_de_tarefas(client, lista_de_tarefas_pendentes):
+    resp = client.get(reverse('tarefas:home'))
+    return resp
+
+
+def test_lista_de_tarefas_pendetes_presente(resposta_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        assertContains(resposta_com_lista_de_tarefas, tarefa.nome)
